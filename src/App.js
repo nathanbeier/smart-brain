a896faf1c1be4020c845cda180035bd5ec412a2f
nathanbeier <nathan.beier@outlook.com>
2018-03-16 00:28:47 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -76,16 +76,17 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://morning-tor-53672.herokuapp.com//imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
               input: this.state.input
             })
           })
+            .then(response => response.json)
             .then(response => {
               if (response) {
-                fetch('http://localhost:3000/image', {
+                fetch('https://morning-tor-53672.herokuapp.com/image', {
                 method: 'put',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://morning-tor-53672.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
